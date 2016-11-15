1b5d3124d0350fb0aa242441d77e27edcd66fa57
Franke <vedran.franke@gmail.com>
2016-11-15 10:53:18 +0100

    Corrected function for running MACS2

--- R/Peak_Calling.R
+++ R/Peak_Calling.R
@@ -1,16 +1,23 @@
-callPeaks_MACS2 = function(chipfile=NULL, contfile=NULL, outpath, name, genome='hs'){
+# ---------------------------------------------------------------------------- #
+call_MACS2 = function(chipfile=NULL, contfile=NULL, outpath, name, genome='hs'){
     
+    outdir = file.path(outpath, name)
+        dir.create(outdir, showWarnings=FALSE)
+        
     if(is.null(chipfile))
         stop('Please specify the ChIP file')
     
-    command = paste('MACS2 callpeaks',
+    command = paste('macs2 callpeak',
                     '-t', chipfile,
                     '-g', genome,
-                    -'n', name,
+                    '-n', name,
                     '-f', 'BAM',
                     '--call-summits',
-                    '--outdir', outpath)
+                    '--outdir', outdir)
     if(!is.null(contfile))
         command = paste(command, '-t', contfile)
     system(command)
 }
+
+# ---------------------------------------------------------------------------- #
+
