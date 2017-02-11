933ef2083523eda7637b7bd36759426f600d8503
Franke <vedran.franke@gmail.com>
2017-02-11 16:03:40 +0100

    read_ERCC modularization

--- R/Read_ERCC.R
+++ R/Read_ERCC.R
@@ -1,14 +1,19 @@
-ercc.annot.path = '/data/akalin/Base/ERCC/Annotation/ERCC92.txt'
-read_ERCC = function(ercc.bamfiles){
+read_ERCC = function(ercc.bamfiles, ercc.annot.path){
 
+  library(Rsamtools)
 		ercc.cnts = lapply(ercc.bamfiles, function(x){
 													name = BamName(x)
-													d = data.frame(do.call(rbind, strsplit(system(paste('samtools idxstats',x), intern=TRUE),'\\t')))[,c(1,3)]
+                          d = idxstatsBam(x)[,c(1,3)]
 													colnames(d) = c('id',name)
 													data.table(d)
 													})
 		ercc.cnts = MergeDataTable(ercc.cnts,'id')
 		ercc.cnts = ercc.cnts[-1,]
+
+    stat = Rsamtools::idxstatsBam(ercc.bamfiles[1])[,c('seqnames','seqlength')]
+    ercc.cnts = merge(stat, dbam, by.x='seqnames', by.y='id')
+    setnames(ercc.cnts, 'seqnames', 'id')
+
 		ercc.annot = read.table(ercc.annot.path, header=TRUE, sep='\t')[,c(2,3,4,5)]
 		colnames(ercc.annot) = c('id','subgroup','cont.mix1','cont.mix2')
 		ercc.all = merge(ercc.annot, ercc.cnts, by='id')
