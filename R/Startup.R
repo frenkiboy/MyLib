37ad458ab8a3f9070925f6e95c3ece086f8e5882
Franke <vedran.franke@gmail.com>
2017-02-15 12:58:27 +0100

    Added glob as a global list container

--- R/Startup.R
+++ R/Startup.R
@@ -8,6 +8,7 @@ hh <- function(d) d[1:5,1:5]
 
 ln <- function(x)length(unique(x))
 
+glob = list()
 
 # ------------------------------------------------------------------- #
 # calculates the memory usage of objects
