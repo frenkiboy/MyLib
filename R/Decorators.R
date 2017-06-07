ae5a7b90ba76518620a4a5824077662d9e534ac4
Franke <vedran.franke@gmail.com>
2017-06-08 01:11:49 +0200

    Decorate functions from klmr + decorator function for saving temp files

---
+++ R/Decorators.R
@@ -0,0 +1,32 @@
+# ------------------------------------------------------------------ #
+#' checkLoad - function decorator to save function output
+#' checkLoad is a function decorator which saves the output of a function to an RDS object
+#' TODO
+#' [] add hash sum check for object and parameters
+#'
+#'
+#' @param f function to be executed
+#' @param inpath location where to save the function output
+#' @param load boolean, whether to load the saved output or force execution
+
+#' @return f function output
+#' @export
+checkLoad = function(f, inpath='./'){
+
+    library(digest)
+    fname = deparse(substitute(f))
+    function(..., load=TRUE){
+        print(match.call())
+        outfile = file.path(inpath,paste(fname,'rds',sep='.'))
+        if(load && file.exists(outfile)){
+            message('Returning loaded data ...')
+            return(readRDS(outfile))
+       }else{
+           message('Running function ...')
+            dat = f(...)
+
+            saveRDS(dat, outfile)
+            dat
+        }
+    }
+}
