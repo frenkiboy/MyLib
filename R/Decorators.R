1c3f789d5e7ffe49b0269d266e542b97cbdcf6fd
Franke <vedran.franke@gmail.com>
2017-07-26 17:48:46 +0200

    cacheFile: caches also function body

--- R/Decorators.R
+++ R/Decorators.R
@@ -17,27 +17,34 @@
 #' g = cacheFile('./') %@% f
 #' g(2,3)
 #' g(2,3)
-#' 
+#'
 #' g(x=2,y=3)
 #' g(x=2,y=3)
+
+#' f = function(x=1,y=2){x;print(y);x+y}
+#' g = cacheFile('./') %@% f
+#' g(2,3)
+
+
 source(file.path(lib.path, 'Decorate.R'))
 cacheFile = function(inpath)decorator %@% function(f){
 
-    library(digest)       
+    library(digest)
     argnames = head(as.list(args(as.list(environment())[[1]])),-1)
-    function(..., load=TRUE, .anames = argnames){
-        
+    body = as.list(body(f))
+    function(..., load=TRUE, .anames = argnames, .fbody=body){
+
         fcall = as.list(match.call())
         fname = fcall[[1]]
         args  = head(fcall[-1],-1)
-        
+
         # finds out the function arguments and creates the argument hash
         if(!is.null(names(args))){
             named_args = setdiff(names(args),'')
             if(!is.null(named_args))
                 for(i in named_args)
                     .anames[[i]] = args[[i]]
-            
+
             pos_args = which(names(args) == '')
             if(length(pos_args) > 0)
                 for(i in pos_args)
@@ -46,8 +53,9 @@ cacheFile = function(inpath)decorator %@% function(f){
             for(i in seq_along(args))
                 .anames[[i]] = args[[i]]
         }
-        args_hash = digest(.anames)
-            
+        hashlist = list(anames = .anames, body=.fbody)
+        args_hash = digest(hashlist)
+
         outfile = file.path(inpath,paste(fname, args_hash, 'rds', sep='.'))
         if(load && file.exists(outfile)){
             print(paste0(fname,': Returning loaded data ...'))
@@ -57,11 +65,8 @@ cacheFile = function(inpath)decorator %@% function(f){
             print(paste0(fname,': Running function ...'))
             dat = f(...)
 
-            saveRDS(list(dat=dat, args=.anames), outfile)
+            saveRDS(list(dat=dat, args=.anames, body=.fbody), outfile)
             dat
         }
     }
 }
-
-
-
