424e31f8575fa48707a95fde8ebcc0cb5fd0f207
Franke <vedran.franke@gmail.com>
2017-11-12 15:55:03 +0100

    Motif_Functions.R - Changed scanning results to bam output

--- R/Motif_Functions.R
+++ R/Motif_Functions.R
@@ -1,14 +1,14 @@
 # ---------------------------------------------------------------------------- #
 parse_MEMEdb = function(infile, type='log2probratio', bg=c(A=.25,C=.25,G=.25,G=.25)){
-    
+
     library(stringr)
     library(TFBSTools)
     r = readLines(infile)
     mind = which(str_detect(r, 'MOTIF'))
-    
+
     matlist = list()
     for(i in 1:length(mind)){
-        
+
         cat(i,'\r')
         ind = mind[i]
         name = unlist(strsplit(r[ind],' '))
@@ -21,13 +21,13 @@ parse_MEMEdb = function(infile, type='log2probratio', bg=c(A=.25,C=.25,G=.25,G=.
         mat = round(mat*100)
         mat = apply(mat,2,as.integer)
         rownames(mat) = c('A','C','G','T')
-        
-        
+
+
         pfm = PFMatrix(profileMatrix=mat, ID=name[2], name=name[3],
                        matrixClass='transcription factor')
         pwm = toPWM(pfm, type=type, bg=bg)
         matlist[[name[2]]] = pwm
-        
+
     }
     return(matlist)
 }
@@ -44,54 +44,69 @@ extract_Jaspar_Motifs = function(organism, database='JASPAR2017'){
 
 
 # ---------------------------------------------------------------------------- #
-scan_Genome = function(matlist, genome.name, outpath, min.score='80%', ncores=16, remove=FALSE){
-    
+scan_Genome = function(
+  matlist,
+  genome.name,
+  outpath,
+  min.score = '80%',
+  ncores    = 16,
+  remove    = FALSE
+){
+
     library(doMC)
+    suppressPackageStartupMessages(library(GenomicAlignments))
     library(TFBSTools)
     library(stringr)
-    
+
     source(file.path(lib.path, 'FileLoader.R'), local=TRUE)
     gname = str_replace(genome.name,'^.+\\.','')
     path_out_scan_genome = file.path(outpath, gname)
     dir.create(path_out_scan_genome, showWarnings=FALSE)
-    
-    
+
+
     genome = GenomeLoader(genome.name)
     chrs = names(genome)
     chrs = chrs[!str_detect(chrs, 'Un')]
     chrs = chrs[!str_detect(chrs, 'random')]
     chrs = chrs[!str_detect(chrs, 'hap')]
     chrs = setdiff(chrs, c('chrM','chrY'))
-    
+
     gl = lapply(chrs, function(x)genome[[x]])
     names(gl) = chrs
     gl = DNAStringSet(gl)
-    
+
     registerDoMC(ncores)
     donefiles = as.character(list.files(path_out_scan_genome))
     if(remove)
         donefiles = vector()
-    
+
     foreach(m = 1:length(matlist), .errorhandling='remove')%dopar%{
         print(m)
         mat = matlist[[m]]
         name = ID(mat)
-        outname = paste(name, 'ms',str_replace(min.score,'%',''), 'rds', sep='.')
+        outname = paste(name, 'ms',str_replace(min.score,'%',''), 'bam', sep='.')
         if((length(donefiles)==0) || (!outname %in% donefiles)){
-            
+
             print(name)
             hits.p  = searchSeq(mat, gl, strand='+', min.score=min.score)
             hits.m  = searchSeq(mat, gl, strand='-', min.score=min.score)
             ghits.p = try(unlist(GRangesList( lapply(hits.p, function(x)as(x,'GRanges')))))
             ghits.m = try(unlist(GRangesList( lapply(hits.m, function(x)as(x,'GRanges')))))
             ghits = GRanges()
-            if(!is.na(ghits.p))
+            if(!is.null(ghits.p))
                 ghits = c(ghits, ghits.p)
-            if(!is.na(ghits.m))
+            if(!is.null(ghits.m))
                 ghits = c(ghits, ghits.m)
-            
-            if(length(ghits) > 1)
-                saveRDS(ghits, file.path(path_out_scan_genome, outname))
+
+            seqlengths(ghits) = seqlengths(genome)[seqlevels(ghits)]
+            genome(ghits) = genome.name
+
+
+            ga = as(ghits, 'GAlignments')
+            colnames(values(ga))[3] = 'AS'
+            colnames(values(ga))[4] = 'RS'
+            colnames(values(ga))[8] = 'seq'
+            rtracklayer::export(ga, con=BamFile(file.path(path_out_scan_genome, outname)))
         }
     }
 }
@@ -107,7 +122,7 @@ parse_ScanGenome = function(inpath, regions, ncores=16){
     }else{
         scanfiles = inpath
     }
-    
+
   registerDoMC(ncores)
   scan.list = list()
   scan.list = foreach(i = 1:length(scanfiles), .inorder=FALSE)%dopar%{
