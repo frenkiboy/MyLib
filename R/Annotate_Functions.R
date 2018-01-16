66d479f33dc7bef1753503610896d156a23006fd
Franke <vedran.franke@gmail.com>
2018-01-16 10:57:39 +0100

    Annotate_Reads: removed print(chr); add two messages

--- R/Annotate_Functions.R
+++ R/Annotate_Functions.R
@@ -24,16 +24,16 @@
 
 
 annotate_Antisense = function(
-    regs, 
-    gtf, 
-    tss.up          = 1000, 
-    tss.down        = 1000, 
+    regs,
+    gtf,
+    tss.up          = 1000,
+    tss.down        = 1000,
     tts.down        = 1000,
     tss.dist.param  = 2000,
     antisense.annot = FALSE){
 
     library(GenomicRanges)
-    
+
     if(class(gtf) != 'GRanges' ){
         if(class(gtf) == 'list' && 'gtf' %in% names(gtf)){
             gtf.sel = gtf$gtf
@@ -41,27 +41,27 @@ annotate_Antisense = function(
         }else{
             stop('GTF is not in a recognized format')
         }
-            
+
     }else{
         gtf.sel = gtf
         annot   = as.data.frame(values(gtf.sel))
         annot   = annot[,c('gene_name','gene_id','transcript_id')]
         annot   = unique(data.table(annot))
     }
-  
+
     genes = unlist(range(split(gtf.sel, gtf.sel$gene_id)))
     trans = unlist(range(split(gtf.sel, gtf.sel$transcript_id)))
-    
+
     tss = promoters(resize(trans, fix='start', 1), upstream=tss.up, downstream=tss.down)
     tts = promoters(resize(trans, fix='end', 1),   upstream=0, downstream=tts.down)
-    
+
     regs.tss = resize(regs, width=1, fix='start')
     regs.tss.anti = invertStrand(regs.tss)
-  
+
     nreg = nchar(length(regs)) + 1
-    ids  = paste0('Anti%0',nreg,'d') 
+    ids  = paste0('Anti%0',nreg,'d')
     regs$antisense_id = sprintf(ids, 1:length(regs))
-    
+
     # ------------------------------------------------------------------------ #
     message('Reathrough...')
         regs$readthrough = countOverlaps(regs.tss, tts) > 0
@@ -72,7 +72,7 @@ annotate_Antisense = function(
 
     # ------------------------------------------------------------------------ #
     message('Gene Body Antisense...')
-        ds.gb.anti = .get_Region_Annotation(regs.tss.anti, genes, annot, 'gene_body_anti', 
+        ds.gb.anti = .get_Region_Annotation(regs.tss.anti, genes, annot, 'gene_body_anti',
                                         merge.by='gene_id')
 
     # ------------------------------------------------------------------------ #
@@ -82,8 +82,8 @@ annotate_Antisense = function(
     # ------------------------------------------------------------------------ #
     message('Convergent - Divergent ... ')
         regs = Annotate_Divergent_Convergent(
-            regs, gtf, antisense.annot, tss.dist.param = tss.dist.param)    
-    
+            regs, gtf, antisense.annot, tss.dist.param = tss.dist.param)
+
     ds.all = cbind(ds.tss.anti, ds.gb.anti, ds.tss)
     values(regs) = cbind(values(regs), ds.all)
     return(regs)
@@ -94,20 +94,20 @@ annotate_Antisense = function(
 
 # ---------------------------------------------------------------------------- #
 Annotate_Divergent_Convergent = function(
-    rsa, 
-    gtf = NULL, 
+    rsa,
+    gtf = NULL,
     antisense.annot = FALSE,
     tss.dist.param = 2000
 ){
-   
+
     if(is.null(gtf))
         stop('GTF file is not specified')
-     
+
     library(GenomicRanges)
     message('Preparing annotation ...')
         rsa.tss = resize(rsa, width=1, fix='start')
         rsa.tss.str = invertStrand(rsa.tss)
-     
+
         gtf.l = subset(gtf, type=='exon')
         gtf.l = unlist(reduce(split(gtf.l, gtf.l$gene_id)))
         sl = seqlevels(gtf.l)
@@ -117,11 +117,11 @@ Annotate_Divergent_Convergent = function(
         sl = sl[!str_detect(sl,'HG')]
         sl = sl[!str_detect(sl,'MT')]
         seqlevels(gtf.l, pruning.mode='coarse') = sl
-    
+
         gtf.pct = range(split(gtf, gtf$transcript_id))
         gtf.pct.tss = unlist(resize(gtf.pct, width=1, fix='start'))
-    
-    message('Annotating  ...')    
+
+    message('Annotating  ...')
         tss.dist      = as.data.table(distanceToNearest(rsa.tss.str, gtf.pct.tss, ignore.strand=FALSE))
         tss.dist$qs   = start(rsa.tss)[tss.dist$queryHits]
         tss.dist$qstr = as.character(strand(rsa.tss))[tss.dist$queryHits]
@@ -133,28 +133,28 @@ Annotate_Divergent_Convergent = function(
         tss.dist$str.dist.log = log10(tss.dist$distance)
         tss.dist$str.dist.log[tss.dist$str.dist < 0] = tss.dist$str.dist.log[tss.dist$str.dist < 0] * -1
         tss.dist$tid = names(gtf.pct.tss)[tss.dist$subjectHits]
-        
+
         tss.dist$type = 'None'
         tss.dist$type[with(tss.dist, str.dist < 0 & abs(str.dist) < tss.dist.param)] = 'Divergent'
         tss.dist$type[with(tss.dist, str.dist > 0 & abs(str.dist) < tss.dist.param)] = 'Convergent'
-    
+
         rsa$nearest.tss.dist = tss.dist$str.dist
         rsa$antisense.type   = tss.dist$type
         rsa$antisense.type[rsa$readthrough]   = 'None'
         rsa$antisense.type[rsa$antisense.type == 'None' & rsa$gene_body_anti.ind] = 'Internal'
-        
+
     if(antisense.annot){
         message('Antisense - Divergent/Convergent ...')
             rsa.flip = subset(flipStrand(rsa.tss), !readthrough)
             rsa.dist = as.data.table(distanceToNearest(rsa.tss, rsa.flip))
             rsa.dist$antisense.type = rsa$antisense.type
-            
+
             rsa.dist$qs   = start(rsa.tss)[rsa.dist$queryHits]
             rsa.dist$qstr = as.character(strand(rsa.tss))[rsa.dist$queryHits]
-            
+
             rsa.dist$ts    = start(rsa.flip)[rsa.dist$subjectHits]
             rsa.dist$tstr = as.character(strand(rsa.flip))[rsa.dist$subjectHits]
-            
+
             rsa.dist$str.dist = rsa.dist$distance
             rsa.dist$str.dist[with(rsa.dist, qs <= ts & tstr == '+' )] = rsa.dist$str.dist[with(rsa.dist, qs <= ts & tstr == '+' )] * -1
             rsa.dist$str.dist[with(rsa.dist, qs >= ts & tstr == '-' )] = rsa.dist$str.dist[with(rsa.dist, qs >= ts & tstr == '-' )] * -1
@@ -165,25 +165,25 @@ Annotate_Divergent_Convergent = function(
             rsa.dist$type[with(rsa.dist, str.dist > 0 & abs(str.dist) < 10000)] = 'A_Convergent'
             rsa.dist$readthrough = rsa$readthrough
             rsa.dist = subset(rsa.dist, antisense.type=='None' & !readthrough)
-            
+
             rsa$antisense.type[rsa.dist$queryHits] = rsa.dist$type
             rsa$antisense.type[rsa.dist$queryHits] = rsa.dist$type
-            
+
             message('Antisense - Internal ...')
             fora = dtfindOverlaps(rsa, rsa.flip, ignore.strand=TRUE)
             fora.dist = as.data.table(distanceToNearest(rsa.tss, rsa.flip))
-            
+
             fora$antisense.type = rsa$antisense.type[fora$queryHits]
             fora$readthrough    = rsa$readthrough[fora$queryHits]
-            fora$qs             = start(rsa)[fora$queryHits]   
+            fora$qs             = start(rsa)[fora$queryHits]
             fora$ts             = start(rsa.flip)[fora$subject]
             fora$dist           = with(fora, abs(qs - ts))
             fora$width          = width(rsa)[fora$queryHits]
             fora$id1            = rsa$antisense_id[fora$queryHits]
             fora$id2            = rsa.flip$antisense_id[fora$subjectHits]
-            
+
             fora = subset(fora, !readthrough & id1 != id2 & antisense.type=='None')
-            
+
             rsa$antisense.type[fora$queryHits] = 'A_Internal'
     }
     return(rsa)
@@ -312,7 +312,7 @@ setMethod("AnnotateRanges",signature("GRanges","GRanges"),
 # ---------------------------------------------------------------------------- #
 # given a gtf file constructs the annotation list
 GTFGetAnnotation = function(g, downstream=500, upstream=1000){
-    
+
     g.exon = g[g$type=='exon']
     exon = unlist(g.exon)
     gene = unlist(range(split(g.exon, g.exon$gene_id)))
@@ -320,15 +320,15 @@ GTFGetAnnotation = function(g, downstream=500, upstream=1000){
     tts  = promoters(resize(gene, width=1, fix='end'), downstream=downstream,
                     upstream=upstream)
     intron = GenomicRanges::setdiff(gene, exon)
-    
+
     values(exon) = NULL
     gl = GRangesList(tss    = tss,
                      tts    = tts,
                      exon   = exon,
                      intron = intron)
-    
+
     return(gl)
-    
+
 }
 
 # ---------------------------------------------------------------------------- #
@@ -340,9 +340,9 @@ Annotate_Reads = function(infile, annotation, ignore.strand=FALSE, ncores=8){
     chrs = chrFinder(infile)
     # chrs = chrs[!chrs$chr %in% c('chrM','chrY'),]
     lchr = list()
+    message('Looping ...')
     lchr = foreach(chr = chrs$chr)%dopar%{
 
-        print(chr)
         w = GRanges(chr, IRanges(1, chrs$chrlen[chrs$chr==chr]))
         reads = readGAlignments(infile, use.names=TRUE, param=ScanBamParam(which=w, tag='NH'))
         g = granges(reads, use.names=TRUE, use.mcols=TRUE)
@@ -357,6 +357,7 @@ Annotate_Reads = function(infile, annotation, ignore.strand=FALSE, ncores=8){
         dg = dg[!duplicated(dg$rname)]
         return(dg)
     }
+    message('Merging ...')
     ldg = rbindlist(lchr)
     ldg = ldg[order(match(ldg$annot, c(names(annotation),'None')))]
     ldg = ldg[!duplicated(ldg$rname)]
@@ -475,22 +476,22 @@ setMethod("Get_Annotation",signature("GRanges"),
               trl.ranges = unlist(range(trl))
 
               dannot = as.data.frame(values(granges))
-              
+
               message('Constructing annotation...')
               # annot = unique(GRangesTodata.frame(granges)[,c('gene_id','transcript_id','gene_name','gene_biotype')])
               vsub = c('gene_id','transcript_id')
               if('gene_name' %in% colnames(dannot))
                   vsub = c(vsub,'gene_name')
-              
+
               if('gene_biotype' %in% colnames(dannot))
                   vsub = c(vsub,'gene_biotype')
-              
+
               annot = unique(dannot[,vsub])
               annot$gcoord = as.character(grl.ranges)[annot$gene_id]
               annot$gwidth = width(grl.ranges)[annot$gene_id]
               annot$tcoord = as.character(trl.ranges)[annot$transcript_id]
               annot$twidth = sum(width(trl))[annot$transcript_id]
-              
+
               return(annot)
 })
 
@@ -500,37 +501,37 @@ setMethod("Get_Annotation",signature("GRanges"),
 #' with their corresponding locations
 #'
 #' @param peaks GRanges with peak location
-#' @param annot annotation file 
+#' @param annot annotation file
 #'
 #' @return annotatet GRanges
 Annotate_Peaks = function(peaks, gtf, exon_id = 'exon', annot_id = 'annot',
                           colnames = c('gene_id','gene_name','gene_biotype','gcoord')){
-    
+
     source(file.path(lib.path, 'Annotate_Functions.R'), local=TRUE)
     source(file.path(lib.path, 'ScanLib.R'), local=TRUE)
     message('Constructing annotation...')
         annot.l = GTFGetAnnotation(gtf[[exon_id]])
-    
-        
+
+
     message('Gene annotation...')
         peaks$gene.annot = suppressWarnings(AnnotateRanges(peaks, annot.l, type='precedence'))
-    
+
      message('Getting gene names...')
         gtf.gens    = subset(gtf[[exon_id]], type=='exon')
         gtf.gens    = unlist(range(split(gtf.gens, gtf.gens$gene_id)))
         fog         = dtfindOverlaps(peaks, resize(gtf.gens, width=width(gtf.gens)+1000, fix='end'))
         fog$gene_id = names(gtf.gens)[fog$subjectHits]
-    
+
         fogm = merge(fog, unique(gtf[[annot_id]][,colnames]), by='gene_id')
         fogm = fogm[,c('queryHits','gene_name','gene_id'),with=FALSE][,lapply(.SD, function(x)paste(unique(x), sep=':', collapse=':')),by='queryHits']
         fog$subjectHits=NULL
-    
+
         peaks$gene_name = 'None'
         peaks$gene_name[fogm$queryHits] = fogm$gene_name
-        
+
         peaks$gene_id = 'None'
         peaks$gene_id[fogm$queryHits]   = fogm$gene_id
-        
+
     # message('Getting Associated Genes...')
     # foa = dtfindOverlaps(peaks, resize(gtf.gens, width=width(gtf.gens)+1000000, fix='center'))
     # foa$gene_id = names(gtf.gens)[foa$subjectHits]
@@ -539,8 +540,8 @@ Annotate_Peaks = function(peaks, gtf, exon_id = 'exon', annot_id = 'annot',
     # foa = foa[,list(list(gene_id)),by=queryHits]
     # peaks$gene_id_assoc = 'None'
     # peaks$gene_id_assoc[foa$queryHits] = foa$V1
-    
+
     return(peaks)
-    
+
 }
 
