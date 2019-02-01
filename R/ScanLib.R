b535816fd3cc5893438ad96247c6c5c6602ab7d8
frenkiboy <vedran.franke@gmail.com>
2019-02-01 10:32:11 +0100

    ggplotColors

--- R/ScanLib.R
+++ R/ScanLib.R
@@ -342,3 +342,8 @@ gzfile = function(infile){
 
 
 #####--------------------/FUNCTIONS/---------------------------#####
+ggplotColors <- function(g){
+  d <- 360/g
+  h <- cumsum(c(15, rep(d,g - 1)))
+  hcl(h = h, c = 100, l = 65)
+}
